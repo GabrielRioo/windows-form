7e860c3153673dbb3cb7e0cf54bf4eec531c3789
GabrielRioo <Gabrielpaivario@gmail.com>
2020-09-28 12:46:01 -0300

    create a dropdown

--- WinForm/Form_ComboBox.Designer.cs
+++ WinForm/Form_ComboBox.Designer.cs
@@ -29,11 +29,13 @@
 		private void InitializeComponent()
 		{
 			this.cb_transporte = new System.Windows.Forms.ComboBox();
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
-			this.button3 = new System.Windows.Forms.Button();
-			this.button4 = new System.Windows.Forms.Button();
-			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.btn_mostrarSelecionado = new System.Windows.Forms.Button();
+			this.btn_limpar = new System.Windows.Forms.Button();
+			this.btn_resetarElementos = new System.Windows.Forms.Button();
+			this.btn_adicionar = new System.Windows.Forms.Button();
+			this.txb_transporte = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lbl_resultado = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// cb_transporte
@@ -49,60 +51,83 @@
 			this.cb_transporte.Name = "cb_transporte";
 			this.cb_transporte.Size = new System.Drawing.Size(295, 21);
 			this.cb_transporte.TabIndex = 0;
+			this.cb_transporte.SelectedIndexChanged += new System.EventHandler(this.cb_transporte_SelectedIndexChanged);
 			// 
-			// button1
+			// btn_mostrarSelecionado
 			// 
-			this.button1.Location = new System.Drawing.Point(313, 12);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(238, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
+			this.btn_mostrarSelecionado.Location = new System.Drawing.Point(313, 12);
+			this.btn_mostrarSelecionado.Name = "btn_mostrarSelecionado";
+			this.btn_mostrarSelecionado.Size = new System.Drawing.Size(238, 23);
+			this.btn_mostrarSelecionado.TabIndex = 1;
+			this.btn_mostrarSelecionado.Text = "Mostrar Selecionado";
+			this.btn_mostrarSelecionado.UseVisualStyleBackColor = false;
+			this.btn_mostrarSelecionado.Click += new System.EventHandler(this.btn_mostrarSelecionado_Click);
 			// 
-			// button2
+			// btn_limpar
 			// 
-			this.button2.Location = new System.Drawing.Point(313, 41);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(238, 23);
-			this.button2.TabIndex = 2;
-			this.button2.Text = "button2";
-			this.button2.UseVisualStyleBackColor = true;
+			this.btn_limpar.Location = new System.Drawing.Point(313, 41);
+			this.btn_limpar.Name = "btn_limpar";
+			this.btn_limpar.Size = new System.Drawing.Size(238, 23);
+			this.btn_limpar.TabIndex = 2;
+			this.btn_limpar.Text = "Limpar Elementos";
+			this.btn_limpar.UseVisualStyleBackColor = true;
+			this.btn_limpar.Click += new System.EventHandler(this.btn_limpar_Click);
 			// 
-			// button3
+			// btn_resetarElementos
 			// 
-			this.button3.Location = new System.Drawing.Point(313, 70);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(238, 23);
-			this.button3.TabIndex = 3;
-			this.button3.Text = "button3";
-			this.button3.UseVisualStyleBackColor = true;
+			this.btn_resetarElementos.Location = new System.Drawing.Point(313, 70);
+			this.btn_resetarElementos.Name = "btn_resetarElementos";
+			this.btn_resetarElementos.Size = new System.Drawing.Size(238, 23);
+			this.btn_resetarElementos.TabIndex = 3;
+			this.btn_resetarElementos.Text = "Resetar Elementos";
+			this.btn_resetarElementos.UseVisualStyleBackColor = true;
+			this.btn_resetarElementos.Click += new System.EventHandler(this.btn_resetarElementos_Click);
 			// 
-			// button4
+			// btn_adicionar
 			// 
-			this.button4.Location = new System.Drawing.Point(313, 146);
-			this.button4.Name = "button4";
-			this.button4.Size = new System.Drawing.Size(238, 23);
-			this.button4.TabIndex = 4;
-			this.button4.Text = "button4";
-			this.button4.UseVisualStyleBackColor = true;
+			this.btn_adicionar.Location = new System.Drawing.Point(313, 146);
+			this.btn_adicionar.Name = "btn_adicionar";
+			this.btn_adicionar.Size = new System.Drawing.Size(238, 23);
+			this.btn_adicionar.TabIndex = 4;
+			this.btn_adicionar.Text = "Adicionar Novo Transporte";
+			this.btn_adicionar.UseVisualStyleBackColor = true;
+			this.btn_adicionar.Click += new System.EventHandler(this.btn_adicionar_Click);
 			// 
-			// textBox1
+			// txb_transporte
 			// 
-			this.textBox1.Location = new System.Drawing.Point(313, 120);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(238, 20);
-			this.textBox1.TabIndex = 5;
+			this.txb_transporte.Location = new System.Drawing.Point(313, 120);
+			this.txb_transporte.Name = "txb_transporte";
+			this.txb_transporte.Size = new System.Drawing.Size(238, 20);
+			this.txb_transporte.TabIndex = 5;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(313, 187);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(0, 13);
+			this.label1.TabIndex = 6;
+			// 
+			// lbl_resultado
+			// 
+			this.lbl_resultado.AutoSize = true;
+			this.lbl_resultado.Location = new System.Drawing.Point(313, 187);
+			this.lbl_resultado.Name = "lbl_resultado";
+			this.lbl_resultado.Size = new System.Drawing.Size(0, 13);
+			this.lbl_resultado.TabIndex = 7;
 			// 
 			// Form_ComboBox
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(563, 450);
-			this.Controls.Add(this.textBox1);
-			this.Controls.Add(this.button4);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
+			this.Controls.Add(this.lbl_resultado);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.txb_transporte);
+			this.Controls.Add(this.btn_adicionar);
+			this.Controls.Add(this.btn_resetarElementos);
+			this.Controls.Add(this.btn_limpar);
+			this.Controls.Add(this.btn_mostrarSelecionado);
 			this.Controls.Add(this.cb_transporte);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.Name = "Form_ComboBox";
@@ -116,10 +141,12 @@
 		#endregion
 
 		private System.Windows.Forms.ComboBox cb_transporte;
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Button button2;
-		private System.Windows.Forms.Button button3;
-		private System.Windows.Forms.Button button4;
-		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button btn_mostrarSelecionado;
+		private System.Windows.Forms.Button btn_limpar;
+		private System.Windows.Forms.Button btn_resetarElementos;
+		private System.Windows.Forms.Button btn_adicionar;
+		private System.Windows.Forms.TextBox txb_transporte;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label lbl_resultado;
 	}
 }
--- WinForm/Form_ComboBox.cs
+++ WinForm/Form_ComboBox.cs
@@ -16,5 +16,50 @@ namespace WinForm
 		{
 			InitializeComponent();
 		}
+
+		private void btn_mostrarSelecionado_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show(cb_transporte.Text);
+		}
+
+		private void btn_limpar_Click(object sender, EventArgs e)
+		{
+			cb_transporte.Items.Clear();
+		}
+
+		private void btn_resetarElementos_Click(object sender, EventArgs e)
+		{
+			cb_transporte.Items.Add("Carro");
+			cb_transporte.Items.Add("Aviao");
+			cb_transporte.Items.Add("Navio");
+			cb_transporte.Items.Add("Onibus");
+			cb_transporte.Items.Add("Trem");
+
+		}
+
+		private void cb_transporte_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			txb_transporte.Text = cb_transporte.Text;
+		}
+
+		private void btn_adicionar_Click(object sender, EventArgs e)
+		{
+			if (txb_transporte.Text != "")
+			{
+				if (cb_transporte.FindString(txb_transporte.Text) < 0)
+				{
+					cb_transporte.Items.Add(txb_transporte.Text);
+					txb_transporte.Clear();
+					txb_transporte.Focus();
+
+					lbl_resultado.Text = "Item adicionado com sucesso";
+				}
+				else
+				{
+					lbl_resultado.Text = "Este item ja existe.";
+				}
+
+			}
+		}
 	}
 }
